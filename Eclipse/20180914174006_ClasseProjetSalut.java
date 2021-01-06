b116fb159ed877660fd50e52625639d32630eda4
Antonin Tritz <45083857+TritzA@users.noreply.github.com>
2021-01-06 02:17:15 -0500

    Add files via upload

---
+++ Eclipse/20180914174006_ClasseProjetSalut.java
@@ -0,0 +1,16 @@
+package packageSalut;
+
+import javax.swing.JOptionPane;
+
+public class ClasseProjetSalut {
+
+	public static void main(String[] args) {
+		//Premier programme
+		// TODO Auto-generated method stub
+
+		///afficher "Salut"
+		String nom =  JOptionPane.showInputDialog("Entez votre nombre");
+		System.out.println("Votre nom  est " + nom);
+	}
+
+}
